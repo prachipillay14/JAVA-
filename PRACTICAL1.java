5b6ca5ffe0e35c29bbe709ffbe4dc72d7bf67104
Prachi Pillay <prchpillay@gmail.com>
2026-02-06 21:16:25 +0530

    PRACTICAL 1
    Executing a simple Java program demonstrating various primitive and non-primitive data types.

---
+++ PRACTICAL1.java
@@ -0,0 +1,16 @@
+public class PRACTICAL1 {
+
+public static void main(String[] args) {
+int rollNo = 14;
+float per = 98.5f;
+double marks = 5.9;
+String name = "prachi";
+boolean isStudent = true;
+System.out.println("Roll Number: " + rollNo);
+System.out.println("Percentage: " + per);
+System.out.println("Marks: " + marks);
+System.out.println("Name: " + name);
+System.out.println("Is Student: " + isStudent);
+}
+}
+
